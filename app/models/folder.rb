be06c8cd17d42c6d801a301defe03cb4c507c279
21mad <ilmir.abyzov@mail.ru>
2022-12-10 13:11:44 +0300

    Added title validations

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,4 +1,5 @@
 class Folder < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy
+  validates_presence_of :name
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :folder
+
+  validates_presence_of :title
 end
