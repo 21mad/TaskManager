0a8ab103eb4757909556566b9d6da4a9a5161b4a
21mad <ilmir.abyzov@mail.ru>
2022-12-17 14:17:14 +0300

    Added email and password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,18 @@ class User < ApplicationRecord
     validates_confirmation_of :password
     validates_uniqueness_of :email
     validates_presence_of :email
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email." } 
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email." } # email validation
+    validates :username, format: { with: /\A(?=.{5,})/x, message: "is too short. It must contain 5 or more characters" }
+
+    PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+    # validates :password, format: { with: PASSWORD_FORMAT, message: "is not secure. Must contain 8 or more characters" }
+    # it works, but I'm too lazy to come up with passwords...
 
     has_many :folders, dependent: :destroy
 end
